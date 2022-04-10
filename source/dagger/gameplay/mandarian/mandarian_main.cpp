f686006357cf189984560be0d5e460d3c481c01a
AndrijaAda99 <andrija.ada99@gmail.com>
2022-04-10 16:42:32 +0200

    Add: Character factory method

--- source/dagger/gameplay/mandarian/mandarian_main.cpp
+++ source/dagger/gameplay/mandarian/mandarian_main.cpp
@@ -6,6 +6,34 @@
 using namespace mandarian;
 using namespace dagger;
 
+Character Character::Get(Entity enity) 
+{
+    auto &reg = Engine::Registry();
+    auto &sprite = reg.get_or_emplace<Sprite>(enity);
+    auto &transform = reg.get_or_emplace<Transform>(enity);
+
+    return Character{ enity, sprite, transform };
+}
+
+Character Character::Create(
+        ColorRGB color_, 
+        Vector2 position_,
+        Vector2 scale_)
+{
+    auto &reg = Engine::Registry();
+    auto entity = reg.create();
+
+    auto character = Character::Get(entity);
+
+    AssignSprite(character.sprite, "EmptyWhitePixel");
+    character.sprite.scale = { scale_ };
+    character.sprite.color = { color_, 1.0f };
+
+    character.transform.position = { position_, 0.0f };
+
+    return character;
+}
+
 void MandarianGame::SetupCamera()
 {
     auto *camera = Engine::GetDefaultResource<Camera>();
@@ -26,4 +54,6 @@ void MandarianGame::WorldSetup()
     ShaderSystem::Use("standard");
 
     SetupCamera();
+
+    auto mandarian = Character::Create();
 }
--- source/dagger/gameplay/mandarian/mandarian_main.h
+++ source/dagger/gameplay/mandarian/mandarian_main.h
@@ -5,10 +5,27 @@
 #include "core/game.h"
 #include "core/engine.h"
 
+#include "core/graphics/sprite.h"
+#include "core/game/transforms.h"
+
 using namespace dagger;
 
 namespace mandarian
 {
+
+    struct Character
+    {
+        Entity entity;
+        Sprite &sprite;
+        Transform &transform;
+
+        static Character Get(Entity);
+
+        static Character Create(
+                ColorRGB color_ = { 1.0f, 1.0f, 1.0f}, 
+                Vector2 position_ = { 0.0f, 0.0f },
+                Vector2 scale = { 10.0f, 10.0f });
+    };
 
     class MandarianGame : public Game
     {
