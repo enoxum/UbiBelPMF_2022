18414190f08549a689602c822c7986c7ef49075b
Isidora Slavkovic <isidora.slavkovic@gmail.com>
2022-04-26 12:58:27 +0200

    Added tile map technique

--- source/dagger/gameplay/mandarian/mandarian_main.cpp
+++ source/dagger/gameplay/mandarian/mandarian_main.cpp
@@ -7,6 +7,10 @@
 #include "mandarian_enemy.h"
 #include "mandarian_camera_focus_system.h"
 
+#include <fstream>
+#include <iostream>
+#include <vector>
+
 using namespace mandarian;
 using namespace dagger;
 
@@ -21,32 +25,52 @@ void MandarianGame::SetupCamera()
     camera->Update();
 }
 
+void AddSprite(Vector2 scale, float tileSize, float zPos, float Space, std::string spriteName, int width, int height, int i, int j) {
+    auto& reg = Engine::Registry();
+    auto entity = reg.create();
+    auto& sprite = reg.get_or_emplace<Sprite>(entity);
+    AssignSprite(sprite, spriteName);
+    sprite.size = scale * tileSize;
+    auto& transform = reg.emplace<Transform>(entity);
+    transform.position.x = (0.5f + j + j * Space - static_cast<float>(width * (1 + Space)) / 2.f) * tileSize;
+    transform.position.y = (0.5f + i + i * Space - static_cast<float>(height * (1 + Space)) / 2.f) * tileSize;
+    transform.position.z = zPos;
+}
+
 void MandarianGame::CreateMap() 
 {
-    auto& reg = Engine::Registry();
+    
     Vector2 scale(1, 1);
-    constexpr int height = 50;
-    constexpr int width = 50;
-    constexpr float tileSize = 20.f;
+    constexpr float tileSize = 30.f;
     float zPos = 1.f;
-    constexpr float Space = 0.0f;
+    float Space = 0.0f;
 
-    // add grass
+    std::string line;
+    std::ifstream myfile("../source/dagger/gameplay/mandarian/map.txt");   
+    std::vector<std::string> lines;
+
+    while (getline(myfile,line)) {
+        lines.push_back(line);
+    }
+    myfile.close();
+
+   
+    int height = lines.size();
+    int width = lines[0].size();
+
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
-            auto entity = reg.create();
-            auto& sprite = reg.get_or_emplace<Sprite>(entity);
-            
-            AssignSprite(sprite, "MiniWorldSprites:Ground:TexturedGrass");
-            sprite.size = scale * tileSize;
-            auto& transform = reg.emplace<Transform>(entity);
-            transform.position.x = (0.5f + j + j * Space - static_cast<float>(width * (1 + Space)) / 2.f) * tileSize;
-            transform.position.y = (0.5f + i + i * Space - static_cast<float>(height * (1 + Space)) / 2.f) * tileSize;
-            transform.position.z = zPos;
-        }     
+            // add grass
+            AddSprite(scale, tileSize, zPos, Space, "MiniWorldSprites:Ground:TexturedGrass", width, height, i, j);
+            // add mandarines
+            if (lines[i][j] == 'M') {
+                AddSprite(scale * 0.5f, tileSize, zPos, Space, "MiniWorldSprites:tangerin", width, height, i, j);
+            }
+        }
     }
-
+    
 }
+
 
 void MandarianGame::CreateEnemies(Entity mandarian)
 {
