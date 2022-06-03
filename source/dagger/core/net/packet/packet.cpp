d3e1b43955cce7b35e9a82fdb2ad771fbc54e2ad
Aleksandar Smigic <smiga287@gmail.com>
2022-06-03 14:47:03 +0200

    Add naive serialization to packet

--- source/dagger/core/net/packet/packet.cpp
+++ source/dagger/core/net/packet/packet.cpp
@@ -1,14 +1,61 @@
 #include "core/net/packet/packet.h"
+#include <algorithm>
 
 using namespace dagger::packet;
 
-Packet dagger::packet::deserialize(UInt8* data_, UInt64 dataLength_) 
+// WARNING: ultra hacky & fragile code ahead
+// packet consists of the raw data and one byte at the end which determines the type inside the variant
+
+Packet packet::deserialize(UInt8* rawData_, UInt64 dataLength_) 
 {
-    return Packet{};
+    Sequence<UInt8> data;
+    data.reserve(dataLength_);
+    std::copy_n(rawData_, dataLength_, data.begin());
+
+    UInt8 variantIdx = 0;
+    data >> variantIdx;
+
+    // TODO: this can probably be better solved with templates
+    if (variantIdx == 0) 
+    {
+        Transform transform;
+        deserialize(data, transform);
+        return Packet{transform};
+    }
+
+    Logger::critical("Unrecognized type of packet for deserialization");
+    return {};
 }
 
-Sequence<UInt8> dagger::packet::serialize(const Packet& packet_)
+Sequence<UInt8> packet::serialize(const Packet& packet_)
 {
-    return Sequence<UInt8>{};
+    Sequence<UInt8> data;
+
+    std::visit([&](auto &&packetData) {
+        serialize(packetData, packet_.var.index(), data);
+    }, packet_.var);
+
+    return data;
 }
 
+void packet::serialize(const Transform& transform_, UInt8 variantIdx_, Sequence<UInt8>& data_) 
+{
+    data_ << transform_.position.x 
+          << transform_.position.y 
+          << transform_.velocity.x
+          << transform_.velocity.y
+          << variantIdx_;
+}
+
+void packet::deserialize(Sequence<UInt8>& data_, Transform& transform_) 
+{
+    data_ >> transform_.velocity.y 
+          >> transform_.velocity.x 
+          >> transform_.position.y 
+          >> transform_.position.x;
+}
+
+Packet::Packet(const Transform& transform_)
+{
+    var.emplace<Transform>(transform_);
+}
--- source/dagger/core/net/packet/packet.h
+++ source/dagger/core/net/packet/packet.h
@@ -2,22 +2,47 @@
 
 #include <variant>
 #include "core/core.h"
+#include "core/net/common/message.h"
+
+#include "core/game/transforms.h"
 
 namespace dagger::packet {
 
-struct Jump {};
-struct Shoot {};
-
 struct Packet 
 {
-    double timestamp;
+    Packet() = default;
+    Packet(const Transform& transform_);
+
+    // TODO: somehow connect NetworkedComponents and this
     std::variant<
-        Jump,
-        Shoot
+        Transform
     > var;
 };
 
-Packet deserialize(UInt8* data_, UInt64 dataLength_);
+Packet deserialize(UInt8* rawData_, UInt64 dataLength_);
 Sequence<UInt8> serialize(const Packet& packet_);
+
+void serialize(const Transform& transform_, UInt8 variantIdx_, Sequence<UInt8>& data_);
+void deserialize(Sequence<UInt8>& data_, Transform& transform_);
+
+template<typename T, IS_TRIVIALLY_COPYABLE(T)>
+Sequence<UInt8>& operator << (Sequence<UInt8>& data_, const T& field)
+{
+    size_t insertionIdx = data_.size();
+    data_.resize(data_.size() + sizeof(T));
+    std::memcpy(data_.data() + insertionIdx, &field, sizeof(T));
+    return data_;
+}
+
+template<typename T, IS_TRIVIALLY_COPYABLE(T)>
+Sequence<UInt8>& operator >> (Sequence<UInt8>& data_, T& field)
+{
+    size_t takeIdx = data_.size() - sizeof(T);
+    std::memcpy(&field, data_.data() + takeIdx, sizeof(T));
+    data_.resize(takeIdx);
+    return data_;
+}
+
+
 
 }
